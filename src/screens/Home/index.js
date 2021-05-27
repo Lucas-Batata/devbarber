ec6298cb39e2289226ddad2f53548b649f9d7288
Lucas Rodrigues <lucasbatata@lucasbatata.com>
2021-05-26 22:08:34 -0300

    Criando tela home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
-import { Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, RefreshControl } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { request, PERMISSIONS } from 'react-native-permissions';
 import Geolocation from '@react-native-community/geolocation';
+
+import Api from '../../Api';
 
 import { 
     Container,
@@ -16,8 +18,11 @@ import {
     LocationInput,
     LocationFinder,
 
-    LoadingIcon
+    LoadingIcon,
+    ListArea
 } from './styles';
+
+import BarberItem from '../../components/BarberItem';
 
 import SearchIcon from '../../assets/search.svg';
 import MyLocationIcon from '../../assets/my_location.svg';
@@ -30,6 +35,7 @@ export default () => {
     const [coords, setCoords] = useState(null);
     const [loading, setLoading] = useState(false);
     const [list, setList] = useState([]);
+    const [refreshing, setRefreshing] = useState(false);
 
     const handleLocationFinder = async () => {
         setCoords(null);
@@ -44,6 +50,7 @@ export default () => {
             setLoading(true);
             setLoadingText('');
             setList([]);
+            getBarbers(); //Eu que coloquei
 
             Geolocation.getCurrentPosition((info)=>{
                 setCoords(info.coords);
@@ -55,12 +62,51 @@ export default () => {
     }
 
     const getBarbers = async () => {
-        
+        setLoading(true);
+        setList([]);
+
+        let lat = null;
+        let lng = null;
+        if(coords){
+            lat = coords.latitude;
+            lng = coords.longitude;
+        }
+
+        let res = await Api.getBarbers(lat, lng, locationText);
+        //console.log(res);
+        if(res.error == ''){
+            if(res.loc){
+                setLocationText(res.loc);
+            }
+
+            setList(res.data);
+
+        } else {
+            alert("Erro: " + res.error);
+        }
+
+        setLoading(false);
     }
     
+    useEffect(()=>{
+        getBarbers();
+    }, []);
+
+    const onRefresh = () => {
+        setRefreshing(false);
+        getBarbers();
+    }
+
+    const handleLocationSearch = () => {
+        setCoords({});
+        getBarbers();
+    }
+
     return (
         <Container>
-            <Scroller>
+            <Scroller refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
 
                 <HeaderArea>
                     <HeaderTitle numberOfLines={2}>Encontre o seu barbeiro favorito</HeaderTitle>
@@ -75,6 +121,7 @@ export default () => {
                         placeholderTextColor="#FFFFFF"
                         value={locationText}
                         onChangeText={t=>setLocationText(t)}
+                        onEndEditing={handleLocationSearch}
                     />
                     <LocationFinder onPress={handleLocationFinder}>
                         <MyLocationIcon width="24" height="24" fill="#FFFFFF" />
@@ -85,6 +132,11 @@ export default () => {
                     <LoadingIcon size="large" color="#FFFFFF" />
                 }   
 
+                <ListArea>
+                    {list.map((item, k)=>(
+                        <BarberItem key={k} data={item} />
+                    ))}
+                </ListArea>
 
             </Scroller>
         </Container>
